ebfb23658a74c8aae27c8ef70feeebaa1a7cc9c5
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-02-16 22:18:13 +0900

    add test

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,10 +1,14 @@
 import XCTest
 @testable import App
+import Vapor
 
 final class HelloWorldServerTests: XCTestCase {
+  let app = Application()
+  var handler: APIProtocol { APIHandler(app: app) }
+  
   func testGreet() async throws {
-    let handler: APIProtocol = APIHandler()
-    let response = try await handler.getGreeting(query: .init(name: "Jane"))
-    XCTAssertEqual(response, .ok(.init(body: .json(.init(message: "Hello, Jane san!")))))
+    let userID = "userID"
+    let response = try await handler.getUserById(query: .init(userID: userID))
+    try XCTAssertEqual(response.ok.body.json.id, userID)
   }
 }
