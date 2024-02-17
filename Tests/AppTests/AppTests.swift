e020798584269a1fc31ed013e0d759878cfd2dac
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-02-17 10:40:02 +0900

    fix test

--- Tests/AppTests/AppTests.swift
+++ Tests/AppTests/AppTests.swift
@@ -1,14 +1,63 @@
 import XCTest
 @testable import App
 import Vapor
+import Fluent
+import FluentMySQLDriver
 
 final class HelloWorldServerTests: XCTestCase {
-  let app = Application()
-  var handler: APIProtocol { APIHandler(app: app) }
+  var handler: any APIProtocol {
+    let app = Application()
+    app.databases.use(
+      .mysql(
+        hostname: Environment.get("DATABASE_HOST")!,
+        username: Environment.get("DATABASE_USERNAME")!,
+        password: Environment.get("DATABASE_PASSWORD")!,
+        database: Environment.get("DATABASE_NAME")!
+      ),
+      as: .mysql
+    )
+
+    return APIHandler(app: app)
+  }
   
-  func testGreet() async throws {
-    let userID = "userID"
-    let response = try await handler.getUserById(query: .init(userID: userID))
-    try XCTAssertEqual(response.ok.body.json.id, userID)
+  func testPostUser() async throws {
+    let user: Components.Schemas.User = .init(
+      id: UUID().uuidString,
+      firstName: "fisrtName",
+      lastName: "lastName",
+      age: 21
+    )
+    
+    let response = try await handler.postUser(.init(body: .json(user)))
+
+    try XCTAssertEqual(response.ok.body.json, user)
+  }
+  
+  func testGetUserById() async throws {
+    let user: Components.Schemas.User = .init(
+      id: UUID().uuidString,
+      firstName: "fisrtName",
+      lastName: "lastName",
+      age: 21
+    )
+    
+    let response1 = try await handler.postUser(.init(body: .json(user)))
+    
+    let response2 = try await handler.getUserById(query: .init(userID: user.id))
+    try XCTAssertEqual(response2.ok.body.json, user)
+  }
+  
+  func testDeleteUserById() async throws {
+    let user: Components.Schemas.User = .init(
+      id: UUID().uuidString,
+      firstName: "fisrtName",
+      lastName: "lastName",
+      age: 21
+    )
+    
+    let response1 = try await handler.postUser(.init(body: .json(user)))
+    let response2 = try await handler.deleteUserByID(.init(query: .init(userID: user.id)))
+
+    try XCTAssertEqual(response2.noContent, .init())
   }
 }
